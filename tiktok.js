22fc89217b58f839e099f9bc17ff48312600b87e
leonardo.perello <leo_16.04.96@hotmail.com>
2020-06-01 00:35:04 -0300

    agregue tiktok terminado

--- tiktok.js
+++ tiktok.js
@@ -1,16 +1,32 @@
-    let n = 10;
-    let t1='Tok', t2='Tik', i;
-    for(i=1; i<=n; i++){
-      if(i % 3 === 0){
-        console.log(t1);
+function run(n) {
+    let i, s = '';
+    for(i=1; i <= n; i++){
+      if(i % 3 === 0 && i % 5 !== 0){
+        s+='Tok';
+        if(i!==n){
+          s+='\n';
+        }
       }
-      else if(i % 5 === 0 & i % 3 !== 0){
-        console.log(t2);
+      else if((i % 5 === 0) && (i % 3 !== 0)){
+        s+= 'Tik';
+        if(i!==n){
+          s+='\n';
+        }
       }
-      else if(i % 5 === 0 % i % 3 === 0){
-        console.log(t2, t1);
+       if((i % 3 === 0) && (i % 5 === 0)){
+        s+= 'TikTok';
+        if(i!==n){
+          s+='\n';
+        }
+      }
+      if((i % 3 !== 0) && (i % 5 !== 0)){
+        s+=i;
+        if(i!==n){
+        s+='\n';
+      }
       }
     }
-    document.write("a");
- 
-  
+    return s;
+    }
+
+console.log(run(50));
